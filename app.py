f67ec87a269fc51727e2e2ce7743f61879fa76a8
Kensley <kensleyw@gmail.com>
2024-01-08 18:25:28 -0300

    alteracao para exibir chat_message

--- app.py
+++ app.py
@@ -4,20 +4,14 @@ import tempfile
 from process_text import question_answer
 from datetime import datetime
 
+#formato_ptb = "%d/%m/%Y %H:%M:%S"
+formato_ptb = "%H:%M:%S"
 
 # Configurações da página
 st.set_page_config(
     page_title="LangChain - Perguntas e Respostas - Q&A",
     layout='wide'
 )
-
-# Define a custom HTML template with a script to scroll the text area
-scroll_script = """
-<script>
-  var textArea = document.getElementById('text_area_chat');
-  textArea.scrollTop = textArea.scrollHeight;
-</script>
-"""
 
 # Sidebar
 st.sidebar.write("**Configurações**")
@@ -30,27 +24,25 @@ chain_type = st.sidebar.radio("Tipo Chain", options=['stuff', 'map_reduce', 'ref
 st.header("LangChain :speech_balloon: - Perguntas e Respostas - Q&A", divider='gray')
 
 if 'historico' not in st.session_state:
-    st.session_state.historico = [f"{datetime.now().__format__('%d/%m/%Y %H:%M:%S')} 😊 Olá. Sou seu assistente virtual ChatBot. Como posso te ajudar?"]
-    
-st.text_area("Chat", value="".join(st.session_state.historico), height=400, key='text_area_chat')
+    st.session_state.historico = [{"role" : "assistant", "content" : f"{datetime.now().__format__(formato_ptb)} Olá. Sou seu assistente virtual ChatBot. Como posso te ajudar?"}]
 
+for h in st.session_state.historico:
+    st.chat_message(h["role"]).write(h["content"])
 
 # Entrada de texto para a pergunta
-pergunta = st.text_input(label="Envie sua pergunta", placeholder="Coloque aqui a sua pergunta")
-
-# Botão para enviar
-btn_enviar = st.button("Enviar")
+pergunta = st.chat_input(placeholder="Coloque aqui a sua pergunta")
 
 #função que preenche o histórico do chat
-if (btn_enviar and pergunta and api_key):
-    data_hora_pergunta = datetime.now().__format__("%d/%m/%Y %H:%M:%S")
-    if len(api_key) == 0:
-        st.sidebar.error("'OpenAI API Key' é obrigatório.")
-        
-    else:
-        
-        with st.spinner("Em processamento"):
-        
+if pergunta:
+    data_hora_pergunta = datetime.now().__format__(formato_ptb)
+
+    with st.spinner("Em processamento"):
+        st.session_state.historico.append({"role" : "user", "content" : f"{data_hora_pergunta} {pergunta}"})
+
+        if len(api_key) == 0:
+            st.session_state.historico.append({"role" : "assistant", "content" : f"{data_hora_pergunta} **ATENÇÃO**, Para utilizar o chat é necessário preencher a *OpenAI API Key*!"})
+
+        else:            
             temp_file_path = os.path.join(tempfile.gettempdir(), "temp.pdf")
 
             with open(temp_file_path, "wb") as temp_file:
@@ -68,15 +60,10 @@ if (btn_enviar and pergunta and api_key):
                 st.error(f"Ocorreu um erro interno: {e}") 
                 
             # Atualiza a variável chat apenas com a última pergunta
-            data_hora_resposta = datetime.now().__format__("%d/%m/%Y %H:%M:%S")
-            st.session_state.historico.insert(0, f"{data_hora_resposta} 😊 Atendente: {resultados['result']}\n\n{data_hora_pergunta} 🌲 Usuário: {pergunta}\n\n")
-            
-            # Inclua o script para rolar a área de texto até o final
-            st.markdown(scroll_script, unsafe_allow_html=True)
-            
+            data_hora_resposta = datetime.now().__format__(formato_ptb)
+            st.session_state.historico.append({"role" : "assistant", "content" : f"{data_hora_resposta} {resultados['result']}"})
+                    
             # atualiza a tela para exibir a resposta no histórico da conversa
-            st.rerun()
-            
-        #limpa o campo pergunta
+        st.rerun()
 
 
